57d51f92a82c473b55eed2eff5c118ed90ac4156
rudiejd <46619169+rudiejd@users.noreply.github.com>
2019-01-12 14:55:10 -0500

    main file for bot

---
+++ bot.py
@@ -0,0 +1,67 @@
+# bot that uses markovify to model how we speak in a group chat
+# jd rudie 2018
+
+import requests
+import markovify
+import time
+import re
+
+
+# train the bot on the message file using markovify
+def gm_train():
+    learning_file = open('gm_msgs.txt', 'r')
+    gm_model = markovify.Text(learning_file)
+    learning_file.close()
+    return gm_model
+
+
+# send generated message to group
+def message(text):
+    print(text)
+    post_params = {'bot_id': 'YOUR_BOT_ID_HERE', 'text': text}
+    requests.post('https://api.groupme.com/v3/bots/post', params=post_params)
+
+
+# main method that checks for messages
+def main():
+    arr = []
+    while True:
+        f = open('gm_msgs.txt', 'a')
+
+        request_params = {'token': 'YOUR_ACCESS_TOKEN_HERE', 'limit': 1}
+        api_return = requests.get('https://api.groupme.com/v3/groups/YOUR_GROUP_ID_HERE/messages', params=request_params).json()
+        response_message = str(api_return['response']['messages'][0]['text'])
+        sender = api_return['response']['messages'][0]['name']
+
+        at_pattern = re.compile('@[A-z]+') # matching all "@(user) references so they don't end up in the training text
+        url_pattern = re.compile('(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))")
+        emoji_pattern = re.compile("["
+                                   u"\U0001F600-\U0001F64F"  # emoticons
+                                   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                   u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                   "]+", flags=re.UNICODE)
+
+        response_message = emoji_pattern.sub(r'', response_message)
+        response_message = url_pattern.sub(r'', response_message)
+        response_message = at_pattern.sub(r'', response_message)
+
+        if not re.match('Greg', sender):
+
+            if re.match('.*[G|g]reg.*', response_message):
+                print("Greg is needed!")
+                model = gm_train()
+                send_gm = model.make_short_sentence(200)
+                arr.append(send_gm)
+                message(send_gm)
+
+            if response_message not in arr:
+                f.write(str(response_message) + ' ')
+                f.close()
+                arr.append(response_message)
+
+            if re.match('.*[T|t]est.*', response_message):
+                message("i don't like tests, they're scary")
+
+
+main()
