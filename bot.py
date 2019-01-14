ca9de445b3ddfcf2600d0dfc0cfe795ffa713d57
rudiejd <46619169+rudiejd@users.noreply.github.com>
2019-01-13 20:41:48 -0500

    more comments, added mostwords command

--- bot.py
+++ bot.py
@@ -3,14 +3,14 @@
 
 import requests
 import markovify
-import time
 import re
+import operator
 
 
-# train the bot on the message file using markovify
+# train the bot on corpus file written from group using markovify
 def gm_train():
     learning_file = open('gm_msgs.txt', 'r')
-    gm_model = markovify.Text(learning_file)
+    gm_model = markovify.Text(learning_file, state_size=2)
     learning_file.close()
     return gm_model
 
@@ -22,19 +22,44 @@ def message(text):
     requests.post('https://api.groupme.com/v3/bots/post', params=post_params)
 
 
+def most_words():
+    with open('gm_msgs.txt', 'r') as f:
+        sf = f.read().strip().replace('@', '').split(' ')
+
+    words = {}
+    boring_stuff = ["these", "what", "are", "can", "make", "not", "him", "in", "of", "he", "she", "that", "this", "it",
+                    "a", "the", "I", "you", "and", "then", "to", "so", "him", "his", "their", "theirs", "its", "we",
+                    "me", "i", "on", "if", "is", "for", "me", "my", "mine", "all", "was", "it's", "what", "are", "can",
+                    "but", "He", "say", "Bing", "do", "be"]
+
+    for word in sf:
+        if word not in words and re.match(r'\w+', word) and word not in boring_stuff:
+            words[word] = 1
+        elif word in words:
+                words[word] += 1
+        else:
+            continue
+    f.close()
+    sorted_words = sorted(words.items(), key=operator.itemgetter(1))
+    return sorted_words[-15:]
+
+
 # main method that checks for messages
+
 def main():
     arr = []
+    #blacklisting the bot itself and the GORT bot from writing to bot's corpus
+    black_list = ('Greg, GORT')
     while True:
-        f = open('gm_msgs.txt', 'a')
 
-        request_params = {'token': 'YOUR_ACCESS_TOKEN_HERE', 'limit': 1}
+
+        request_params = {'token': 'YOUR_TOKEN_HERE', 'limit': 1}
         api_return = requests.get('https://api.groupme.com/v3/groups/YOUR_GROUP_ID_HERE/messages', params=request_params).json()
         response_message = str(api_return['response']['messages'][0]['text'])
         sender = api_return['response']['messages'][0]['name']
 
-        at_pattern = re.compile('@[A-z]+') # matching all "@(user) references so they don't end up in the training text
-        url_pattern = re.compile('(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))")
+        at_pattern = re.compile('@')
+        url_pattern = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+] |[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
         emoji_pattern = re.compile("["
                                    u"\U0001F600-\U0001F64F"  # emoticons
                                    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
@@ -45,23 +70,38 @@ def main():
         response_message = emoji_pattern.sub(r'', response_message)
         response_message = url_pattern.sub(r'', response_message)
         response_message = at_pattern.sub(r'', response_message)
-
-        if not re.match('Greg', sender):
-
+        
+        #checks whether sender is in blacklisted users before running command or recording text
+        if sender not in black_list:
+            #commands for the bot, it only runs one at a time
             if re.match('.*[G|g]reg.*', response_message):
                 print("Greg is needed!")
                 model = gm_train()
                 send_gm = model.make_short_sentence(200)
                 arr.append(send_gm)
                 message(send_gm)
+                    
+            elif re.match('.*[T|t]est.*', response_message):
+                message("shut the fuck up teddy or teddy's bot")
+                arr.append(response_message)
 
-            if response_message not in arr:
+            #give the most used words in a given chat
+            elif re.match('.*mostwords.*', response_message):
+                most = most_words()
+                ret_msg = ""
+                message('Count of most used words in this chat:')
+                for item in most:
+                    ret_msg += item[0] + ': ' + str(item[1]) + '\n'
+
+                message(ret_msg)
+
+            #record a message if it hasn't already been said since the program has been running
+            if response_message not in arr and not re.match('.*[G|g]reg.*', response_message):
+                f = open('gm_msgs.txt', 'a')
                 f.write(str(response_message) + ' ')
                 f.close()
                 arr.append(response_message)
 
-            if re.match('.*[T|t]est.*', response_message):
-                message("i don't like tests, they're scary")
-
 
 main()
+
